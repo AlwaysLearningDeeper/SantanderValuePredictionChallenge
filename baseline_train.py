c51eaf62ed4ad14cb3116cbd18937e19670f9005
Salvador Marti Roman <salvador19marti@gmail.com>
2018-06-21 17:22:20 +0100

    1.94 score: Changed the number of estimators to iterate betweeen 10 to 300 and did the prediction with the best result. baseline_train now separates the test set in a test and training set.

--- baseline_train.py
+++ baseline_train.py
@@ -1,15 +1,28 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 trainX = pd.read_pickle("trainX.pkl")
 trainY = pd.read_pickle("trainY.pkl")
 testX = pd.read_pickle("testX.pkl")
 testIDs = pd.read_pickle("testID.pkl")
 
+trainX, trainXtest, trainY, trainYtest = train_test_split(trainX, trainY, test_size=0.2, random_state=7)
 model = RandomForestRegressor(n_jobs=-1)
 
-model.set_params(n_estimators=100)
+scores = []
+estimators = np.arange(10, 300, 10)
+for n in estimators:
+    model.set_params(n_estimators=n)
+    model.fit(trainX, trainY)
+    scores.append(model.score(trainXtest, trainYtest))
+
+plt.plot(estimators, scores)
+plt.show()
+
+model.set_params(n_estimators=scores.index(max(scores))*10 + 10)
 model.fit(trainX, trainY)
 resultframe = pd.DataFrame()
 resultframe["ID"] = testIDs
